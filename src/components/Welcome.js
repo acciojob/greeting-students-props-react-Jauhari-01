e0a73410a1242be98f9c094a08df76b12fb448fe
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2024-09-24 22:59:10 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,10 +3,10 @@ import React from "react";
 
 const Welcome = ({name}) => {
 return (
-<p>
-<h1>Hey ! {name}</h1>
-<h2>Welcome to School.</h2>
-</p>
+<div>
+<h1>Hey !</h1>
+<p>Welcome to School.</p>
+</div>
 );
 }
 export default Welcome;
